d43bfd3dc1c3d1862a522c07fc24a043e3ffd1bf
Ankit Yadav <ankityadav.webdev@gmail.com>
2024-01-28 16:54:32 +0530

    fix bugs

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import store from './store/store.ts';
-import { Provider } from 'react-redux';
-import { AppContextProvider } from './contexts/AppContext.tsx';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import store from "./store/store.ts";
+import { Provider } from "react-redux";
+import { AppContextProvider } from "./contexts/AppContext.tsx";
 
 export const queryClient = new QueryClient({
     defaultOptions: {
@@ -14,7 +14,7 @@ export const queryClient = new QueryClient({
     },
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
         <Provider store={store}>
             <AppContextProvider>
