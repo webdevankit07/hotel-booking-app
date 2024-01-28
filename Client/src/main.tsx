2127d11570b0cdaf2e87a2bcef184e075eb90134
Ankit Yadav <ankityadav.webdev@gmail.com>
2024-01-28 17:03:53 +0530

    fix bugs

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -14,7 +14,7 @@ export const queryClient = new QueryClient({
     },
 });
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
         <Provider store={store}>
             <AppContextProvider>
