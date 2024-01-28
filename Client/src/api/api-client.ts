fb3da9241dff9bbdc7e23a4d9219a3ce427b6988
Ankit Yadav <ankityadav.webdev@gmail.com>
2024-01-28 16:27:04 +0530

    fix bugs

--- Client/src/api/api-client.ts
+++ Client/src/api/api-client.ts
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const validateToken = async () => {
-    const res = await axios.get('/api/v1/users/auth/validate-token', {
+    const res = await axios.get("/api/v1/users/auth/validate-token", {
         withCredentials: true,
     });
 
-    if (!(res.statusText === 'OK')) {
-        throw new Error('token invalid');
+    if (!(res.statusText === "OK")) {
+        throw new Error("token invalid");
     }
 
     return null;
